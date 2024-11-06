557f7f3faa16cc835ffe8826b0c68f8ab5a6da2c
leandro mortarini <leandromortarini2@gmail.com>
2024-11-06 17:32:58 -0300

    nuevos estilos

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MatButtonModule],
+  imports: [RouterLink, RouterLinkActive, MatButtonModule, MatTabsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
